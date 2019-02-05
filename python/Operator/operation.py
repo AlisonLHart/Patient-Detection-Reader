bcb83f5375745162dc9a9055d7f3700f2572ebbc
dbainter <spacehamster95@gmail.com>
2019-02-04 23:59:40 -0500

    Python bug - if you name a file operator.py it will crash python whenever you try and run it

--- python/Operator/operation.py
+++ python/Operator/operation.py
@@ -1,5 +1,8 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+
+Builder.load_file('operation.kv')
 
 class operatorWindow(BoxLayout):
     def __init__(self, **kwargs):
