e503fdd17ea0d8cc9fee331482a3fc795f1a5efc
dbainter <spacehamster95@gmail.com>
2019-02-10 17:57:05 -0500

    database nonsense

--- python/admin/admin.py
+++ python/admin/admin.py
@@ -1,9 +1,19 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
+from collections import OrderedDict
+from pymongo import MongoClient
+
 class AdminWindow(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        client = MongoClient()
+        db = client.database
+        users = db.users
+
+        for user in users.find():
+            print(user['first_name'])
 
 class AdminApp(App):
     def build(self):
