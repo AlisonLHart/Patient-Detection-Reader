6180ee59b02c02d95b38d4a25fb98a677f1f7dd7
dbainter <spacehamster95@gmail.com>
2019-02-10 16:38:18 -0500

    admin dashboard layout

--- python/admin/admin.py
+++ python/admin/admin.py
@@ -1,16 +1,14 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.lang import Builder
 
-Builder.load_file('admin.kv')
-
-class adminWindow(BoxLayout):
+class AdminWindow(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-class adminApp(App):
+class AdminApp(App):
     def build(self):
-        return adminWindow()
 
-if __name__ =='__main__':
-    adminApp().run
+        return AdminWindow()
+
+if __name__=='__main__':
+    AdminApp().run()
