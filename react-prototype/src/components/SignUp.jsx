d32bf142cf5fc173b9290e6557abe1c2579b2938
dbainter <spacehamster95@gmail.com>
2019-02-24 14:50:56 -0500

    console logging the email and password via the button on signup.jsx

--- react-prototype/src/components/SignUp.jsx
+++ react-prototype/src/components/SignUp.jsx
@@ -1,9 +1,47 @@
 import React, {Component} from 'react'; 
 
 class SignUp extends Component{ // So react is handy dandy in that it's super object oriented. Components are basically objects
+    //https://reactjs.org/docs/components-and-props.html
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+    
+    //helper method
+    signUp(){
+        console.log('this.state', this.state)
+    }
+
     render() {
+        //wow, look at this nonsense
+        //This is the sign in page
         return(
-            <div>SignUp</div>
+            <div className="form-inline">
+                <h2>Sign Up</h2>
+                <div className="form-group">
+                    <input
+                        className="form-control"
+                        type ="text"
+                        placeholder="email"
+                        onChange={event => this.setState({email: event.target.value})}
+                        />
+                    <input
+                        className="form-control"
+                        type ="password"
+                        placeholder="password"
+                        onChange={event => this.setState({password: event.target.value})}
+                        />
+                    <button
+                        className="btn btn-primary"
+                        type = "button"
+                        onClick={() => this.signUp()}>
+                    Sign Up
+                    </button>
+                </div>
+                </div>
         )
     }
 }
