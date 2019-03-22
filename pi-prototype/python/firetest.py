929aa553bfee73469471ff587502ad36cd338ef7
BluElectron <ahartcyber@gmail.com>
2019-03-21 22:24:00 -0400

    PID + RFID SCAN is WORKING

--- pi-prototype/python/firetest.py
+++ pi-prototype/python/firetest.py
@@ -15,35 +15,35 @@ class Fireconnector():
         self.reader = SimpleMFRC522()
         self.patientID = "123"
         self.jsondata = self.fb.get('/patientTest', None)
-        
-        for obj in self.jsondata:
-            print(self.jsondata[obj])
-            print()
 
 
     def scanner(self):
         try:
+            patient = self.getPatient()
+
+
             print("Please scan the appropriate tag")
             tag, text = self.reader.read()
             tagID = str(tag)
-            
-            print("PATIENT ID:" + self.patientID + "\nTAG ID: " + tagID)
+             
+            print("PATIENT ID: " + str(self.patientID) + "\nTAG ID: " + tagID)
 
-            result = self.fb.put('/patientTest/-LaRrBsmee59KjKbEO9A','RFID', tagID)
+            result = self.fb.put('/patientTest/'+ patient,'RFID', tagID)
         finally:
             GPIO.cleanup()
 
     
     def getPatient(self):
         self.patientID = input("Enter the Patient ID: ")
-        for i in range (0, len(self.jsondata)):
-            print(self.jsondata[i][PID])
-            if(self.jsondata[i][PID]==patientID):
-                print(self.jsondata[i])
-                break
-        
+        print(self.jsondata)
+        for obj in self.jsondata:
+            if(self.jsondata[obj]["PID"]==str(self.patientID)):
+                print("FOUND IT\n")
+                return obj
+            else:
+                print("NOT FOUND\n")
 
 
 
 fire = Fireconnector()
-#fire.getPatient()
+fire.scanner()
